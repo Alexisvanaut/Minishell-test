00084c60b2498117e5006564f19af58e69ca0a22
Alexis Vanautgaerden <alvanaut@fu-r4-p1.s19.be>
2025-08-14 17:40:48 +0200

    Wip: rebuild lexer

--- minishell.h
+++ minishell.h
@@ -69,6 +69,13 @@ typedef struct s_command
 	char	**args;
 }	t_command;
 
+
+typedef struct s_parser
+{
+	t_token *current;
+	t_token *tokens;
+}				t_parser;
+
 typedef struct s_ast
 {
 	t_node_type		type;
@@ -80,68 +87,9 @@ typedef struct s_ast
 	struct s_ast	*right;
 }	t_ast;
 
-/*---Debugs utils---*/
 
-void				print_token(t_token *token);
-
-/*---Tokenizer---*/
-
-t_token				*init_token(char *value, t_token_type type);
-void				free_token(t_token *token);
-void				add_token(t_token **token, char *value, t_token_type type);
-int					is_operator_char(char c);
-int					is_operator_len(const char *s);
-char				*parse_quote(const char *s, int *i);
-int					ft_isspace(char c);
-char				*parse_word(const char *s, int *i);
-t_token_type		parse_token_type(const char *op);
-t_token				*tokenize(const char *line);
-char				*parse_operator(const char *s, int *i);
-
-/*---Init structure---*/
-<<<<<<< HEAD
-
-t_command 			*init_command(char *path, char **args);
-t_ast 				*init_ast(t_node_type type, t_command *cmd, int fd_in, int fd_out);
-void 				free_args(char **args);
-void				free_cmd(t_command *cmd);
-void				free_ast(t_ast *ast);
-=======
-
-t_command 			*init_command(char *path, char **args);
-t_ast 				*init_ast(t_node_type type, t_command *cmd, int fd_in, int fd_out);
-void 				free_args(char **args);
-void				free_cmd(t_command *cmd);
-void				free_ast(t_ast *ast);
+/*---lexer---*/
 
 
-/*---Tokens to cmd---*/
-
->>>>>>> refs/remotes/origin/ast
-
-/*---Builtins---*/
-
-<<<<<<< HEAD
-int					is_builtins(t_token *token);
-char				*builtins_path(int builtins);
-int					builtins_size(t_token *token);
-char				**builtins_args(t_token *token);
-int					fill_args_from_tokens(t_token *token, char **args, int start);
-t_command			*token_to_builtin_cmd(t_token *token);
-=======
-
->>>>>>> refs/remotes/origin/ast
-
-/*---Tokens to cmd---*/
-
-
-/*---Built-ins---*/
-int					builtin_echo(int argc,  char **argv);
-int					builtin_cd(int argc,  char **argv);
-int					builtin_pwd(int argc,  char **argv);
-int					builtin_export(int argc,  char **argv);
-int					builtin_unset(int argc,  char **argv);
-int					builtin_env(int argc,  char **argv);
-int					builtin_exit(int argc,  char **argv);
 
 #endif // MINISHELL_H
--- src/lexer.c
+++ src/lexer.c
@@ -1,92 +1,201 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   lexer.c                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: alvanaut <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/06 17:48:33 by alvanaut          #+#    #+#             */
-/*   Updated: 2025/08/06 17:48:36 by alvanaut         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "../minishell.h"
-#include <readline/history.h>
 
-// Analyser l input de readline
-// Regarder le type de l input (separator, word) et lui donner le bon type
-// Pas de split mon seulement des substr, plus simple pour le lexer
-
-char *read_line(void)
+static char *read_line(void)
 {
 	char *line;
-	line = readline("Minishell $");
-	if (!line || !ft_strcmp(line, "exit"))
+
+	line = readline("$Minishell ");
+	if (!line)
 		return (NULL);
+	if (ft_strcmp(line, "exit") == 0)
+		return (free(line), NULL);
+	add_history(line);
 	return (line);
 }
 
-void	free_split(char **tab)
+static char *handle_separator(char *line, int *i)
 {
-	int i;
-
-	i = 0;
-	while (tab[i])
-	{
-		free(tab[i]);
-		i++;
-	}
-	free(tab);
-}
-
-
-char *handle_quote(const char *str)
-{
-	int i;
-	char quote;
-
-	quote = str[0];
-	i = 1;
-	while (str[i])
-	{
-		if (str[i] == quote)
-			return (ft_substr(str, 0, i + 1));
-		i++;
-	}
-	return (printf("Error: syntax error.\n"), NULL);
-}
-
-char	*is_operator(const char *s, int len)
-{
-	if (!s)
+	if (!line)
 		return (NULL);
-	if (ft_strncmp(s, "|", len) == 0 || ft_strncmp(s, "||", len) == 0 || 
-		ft_strncmp(s, "&", len) == 0 || ft_strncmp(s, "&&", len) == 0 ||
-		ft_strncmp(s, ">", len) == 0 || ft_strncmp(s, ">>", len) == 0 ||
-		ft_strncmp(s, "<", len) == 0 || ft_strncmp(s, "<<", len) == 0 ||
-		ft_strncmp(s, "(", len) == 0 || ft_strncmp(s, ")", len) == 0 ||
-		ft_strncmp(s, ";",len) == 0)
-			return (s += len, ft_substr(s, 0, len));
+	if (ft_strncmp(line, "||", 2) == 0)
+		return (*i += 2, ft_substr(line, 0, 2));
+	if (ft_strncmp(line, "|", 1) == 0)
+		return (*i += 1, ft_substr(line, 0, 1));
+	if (ft_strncmp(line, "&&", 2) == 0)
+		return (*i += 2, ft_substr(line, 0, 2));
+	if (ft_strncmp(line, "&", 1) == 0)
+		return (*i += 1, ft_substr(line, 0, 1));
+	if (ft_strncmp(line, ">>", 2) == 0)
+		return (*i += 2, ft_substr(line, 0, 2));
+	if (ft_strncmp(line, ">", 1) == 0)
+		return (*i += 1, ft_substr(line, 0, 1));
+	if (ft_strncmp(line, "<<", 2) == 0)
+		return (*i += 2, ft_substr(line, 0, 2));
+	if (ft_strncmp(line, "<", 1) == 0)
+		return (*i += 1, ft_substr(line, 0, 1));
+	if (ft_strncmp(line, "(", 1) == 0)
+		return (*i += 1, ft_substr(line, 0, 1));
+	if (ft_strncmp(line, ")", 1) == 0)
+		return (*i += 1, ft_substr(line, 0, 1));
+	if (ft_strncmp(line, ";", 1) == 0)
+		return (*i += 1, ft_substr(line, 0, 1));
 	return (NULL);
 }
 
-int		sep_len(const char *s)
+static char	*handle_quote(char *line, int *i)
 {
+	char quote;
+	int	index;
+
+	if (!line)
+		return (NULL);
+	quote = line[0];
+	if (quote != '\'' && quote != '"')
+		  return (NULL);
+	index = 1;
+	while (line[index] && line[index] != quote)
+		index++;
+	if (!line[index])
+		return (printf("Syntax error: unclosed quote\n"), NULL);
+	return (*i += index + 1, ft_substr(line, 1,  index - 1));
+}
+
+static int		is_separator(char *line)
+{
+	if (!line)
+		return (0);
+	if (ft_strncmp(line, "||", 2) == 0 || ft_strncmp(line, "&&", 2) == 0 || ft_strncmp(line, ">>", 2) == 0 ||
+		ft_strncmp(line, "<<", 2) == 0 ) 
+		return (1);
+	if(ft_strncmp(line, ")", 1) == 0 || ft_strncmp(line, "(", 1) == 0 || ft_strncmp(line, "|", 1) == 0 ||
+		ft_strncmp(line, ">", 1) == 0 || ft_strncmp(line, "<", 1) == 0 || ft_strncmp(line, ";", 1) == 0 || 
+		ft_strncmp(line, "&", 1) == 0)
+		return (1);
+	if (line[0] == '\'' || line[0] == '"')
+		return (2);
+	return (0);
+}
+
+static char	*handle_word(char *line, int *i)
+{
+	int index;
+
+	index = 0;
+	while (line[index] && !is_separator(&line[index]))
+	{
+		if (line[index] == '\\' && line[index + 1])
+	  		index++;
+		index++;
+	}
+	if (!line[index])
+		return (*i += index, ft_substr(line, 0, index));
+	return (*i += index, ft_substr(line, 0, index - 1));
+}
+
+void	skip_spaces(char *line, int *i)
+{
+	while (line[*i] == '\t' || line[*i] == ' ')
+		(*i)++;
+}
+
+char	**add_token(char **tokens, char *token)
+{
+	int count;
+	char **new_tokens;
+	int i;
+
+	count = 0;
+	if (!token)
+		return (NULL);
+	if (tokens)
+	{
+		while (tokens[count])
+			count++;
+	}
+	new_tokens = malloc(sizeof(char *) * (count + 2));
+	if (!new_tokens)
+		return (NULL);
+	i = 0;
+	while (i < count)
+	{
+		new_tokens[i] = tokens[i];
+		i++;
+	}
+	new_tokens[count] = ft_strdup(token);
+	new_tokens[count + 1] = NULL;
+	free(tokens);
+	return (new_tokens);
+}
+
+char **lexer(char *line)
+{
+	char *token;
+	char **tokens = NULL;
 	int i;
 
 	i = 0;
-	if (!s)
-		return (0);
-	while (s[i])
+	while (line[i])
 	{
-		if ((s[i] == '|' && s[i + 1] == '|') || (s[i] == '&' || s[i + 1] == '&') ||
-			(s[i] == '>' || s[i + 1] == '>') || (s[i] == '<' || s[i + 1] == '<'))
-			return (2);
-		else if ((s[i] == '|' && s[i + 1] != '|') || (s[i] == '&' || s[i + 1] != '&') ||
-			(s[i] == '>' || s[i + 1] != '>') || (s[i] == '<' || s[i + 1] != '<') || 
-			s[i] == ';')
-			return (1);
-		i++;
+		skip_spaces(line, &i);
+		if (!line[i])
+			break ;
+		if (is_separator(&line[i]) == 1)
+			token = handle_separator(&line[i], &i);
+		else if (is_separator(&line[i]) == 2)
+			token = handle_quote(&line[i], &i);
+		else
+			token = handle_word(&line[i], &i);
+		add_token(tokens, token);
+		free(token);
 	}
-	return (0);
+	return (tokens);
 }
+
+
+//int main(void)
+//{
+//	char *line;
+//    char **tokens = NULL;
+//    char *token;
+//    int i;
+//
+//    while ((line = read_line()))
+//    {
+//        i = 0;
+//        tokens = NULL;
+//
+//        while (line[i])
+//        {
+//            skip_spaces(line, &i);
+//            if (!line[i])
+//                break;
+//
+//            if (is_separator(&line[i]) == 1)
+//                token = handle_separator(&line[i], &i);
+//            else if (is_separator(&line[i]) == 2)
+//                token = handle_quote(&line[i], &i);
+//            else
+//                token = handle_word(&line[i], &i);
+//
+//            tokens = add_token(tokens, token);
+//            free(token);
+//        }
+//
+//        // Afficher les tokens
+//        printf("=== TOKENS ===\n");
+//        for (int j = 0; tokens && tokens[j]; j++)
+//            printf("[%s]\n", tokens[j]);
+//
+//        // Libérer les tokens
+//        if (tokens)
+//        {
+//            for (int j = 0; tokens[j]; j++)
+//                free(tokens[j]);
+//            free(tokens);
+//        }
+//        free(line);
+//    }
+//
+//    return 0;
+//}
+
